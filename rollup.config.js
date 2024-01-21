e6e73ebcfad19d1c13a0693bf1167b3c8aa6d637
7PH <b.raymond@protonmail.com>
2024-01-21 01:13:00 +0100

    Include CJS bundle & Publish v0.2.0

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,28 @@ import esbuild, { minify } from 'rollup-plugin-esbuild';
 const name = 'beautify-url';
 
 const bundle = config => ({
-    ...config,
+    plugins: [esbuild(), minify()],
     input: 'src/index.ts',
     external: id => !/^[./]/.test(id),
+    ...config,
 });
 
 export default [
     bundle({
-        plugins: [esbuild(), minify()],
         output: [
             {
-                file: `dist/${name}.min.js`,
+                file: `dist/${name}.iife.min.js`,
                 name: 'window',
                 extend: true,
                 format: 'iife',
+            },
+        ],
+    }),
+    bundle({
+        output: [
+            {
+                file: `dist/${name}.cjs`,
+                format: 'cjs',
             },
         ],
     }),
